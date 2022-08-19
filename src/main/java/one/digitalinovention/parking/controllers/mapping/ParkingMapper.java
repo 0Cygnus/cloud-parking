854aef14f2558fb4989d48fc3edb3a64ce29cd27
0Cygnus <vinicius.s.2210@gmail.com>
2022-08-19 02:14:24 -0300

    adicionado swagger (alterar config)

--- src/main/java/one/digitalinovention/parking/controllers/mapping/ParkingMapper.java
+++ src/main/java/one/digitalinovention/parking/controllers/mapping/ParkingMapper.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import one.digitalinovention.parking.controllers.dto.ParkingCreateDTO;
 import one.digitalinovention.parking.controllers.dto.ParkingDTO;
 import one.digitalinovention.parking.model.Parking;
 
@@ -14,15 +15,23 @@ public class ParkingMapper {
 	
 	private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 	
-	public ParkingDTO parkingDTO(Parking parking) {
+	public ParkingDTO toParkingDTO(Parking parking) {
 		return MODEL_MAPPER.map(parking, ParkingDTO.class);
 	}
 	
 	public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList){
 		return parkingList
 				.stream()
-				.map(this::parkingDTO)
+				.map(this::toParkingDTO)
 				.collect(Collectors.toList());
+	}
+	
+	public Parking toParking(ParkingDTO dto) {
+		return MODEL_MAPPER.map(dto, Parking.class);
+	}
+	
+	public Parking toParkingCreate(ParkingCreateDTO dto) {
+		return MODEL_MAPPER.map(dto, Parking.class);
 	}
 
 }
--- src/main/java/one/digitalinovention/parking/model/ParkingController.java
+++ src/main/java/one/digitalinovention/parking/model/ParkingController.java
@@ -1,15 +1,26 @@
 package one.digitalinovention.parking.model;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import one.digitalinovention.parking.controllers.dto.ParkingCreateDTO;
 import one.digitalinovention.parking.controllers.dto.ParkingDTO;
 import one.digitalinovention.parking.controllers.mapping.ParkingMapper;
 import one.digitalinovention.parking.service.ParkingService;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Controlador do Estacionamento")
 public class ParkingController {
 
 	private final ParkingService parkingService;
@@ -21,9 +32,27 @@ public class ParkingController {
 	}
 	
 	@GetMapping
-	public List<ParkingDTO> findAll(){
+	@ApiOperation("Buscando Toda a Lista")
+	public ResponseEntity<List<ParkingDTO>> findAll(){
 		List<Parking> parkingList = parkingService.findAll();
 		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-		return result;
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping("/{id}")
+	@ApiOperation("Buscando Por ID")
+	public ResponseEntity<ParkingDTO> finById(@PathVariable String id){
+		Parking parking = (Parking) parkingService.findById(id);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.ok(result);
+	}
+	
+	@PostMapping
+	@ApiOperation("Criando Nova Entrada")
+	public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto){
+		Parking parkingCreate = parkingMapper.toParkingCreate(dto);
+		Parking parking = parkingService.create(parkingCreate);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
 }
